c8b27aab10ab0fed0a5ae9b76525ce17cecffc12
ricpie <ricpie@gmail.com>
2021-05-17 12:48:52 -0600

    parsed timestamp

--- r_scripts/timetracker_import.R
+++ r_scripts/timetracker_import.R
@@ -1,5 +1,6 @@
 timetracker_import_fun <- function(){
   library(jsonlite)
+  library(tidyverse)
   
   tracker_paths <- list.files(
     path = "~/Dropbox/World Bank CSCB Field Folder/Field Data/TimeTracker Data",
@@ -9,7 +10,13 @@ timetracker_import_fun <- function(){
   )
   
   timetracker_data <- rbindlist(lapply(tracker_paths,fromJSON)) %>% 
-    distinct()
-    
+    distinct() %>% 
+    dplyr::mutate(endTime = parse_date_time(endTime,orders = "%a %b %d %H:%M:%S %z %Y",tz = "Africa/Nairobi"),
+                  startTime = parse_date_time(startTime,orders = "%a %b %d %H:%M:%S %z %Y",tz = "Africa/Nairobi")) %>% 
+    dplyr::rename(hhid = user)
   
+  filename = paste0("~/Dropbox/World Bank CSCB Field Folder/Field Data/QA Reports/timetracker_data",  Sys.Date(), ".xlsx")
+  write_xlsx(timetracker_data,filename)
+  
+  return(timetracker_data)
 }
